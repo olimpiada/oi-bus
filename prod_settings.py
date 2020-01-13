080cdec9b54d057c6342d5515b4576ab5dbecab1
Michał Sidor (Michcioperz) <m.sidor@fri.edu.pl>
2020-01-13 15:08:49 +0100

    Update prod_settings.py

--- prod_settings.py
+++ prod_settings.py
@@ -2,24 +2,24 @@ from oi_bus.base_settings import *
 import os
 
 try:
-    with open('/var/db/oi-bus/secret-key') as f:
+    with open('/var/lib/oi-bus/secret-key') as f:
         SECRET_KEY = f.read()
 except FileNotFoundError:
     print('generating a secret key')
     import base64
     SECRET_KEY = base64.encodebytes(os.getrandom(32)).strip()
-    with open('/var/db/oi-bus/secret-key', 'w') as f:
+    with open('/var/lib/oi-bus/secret-key', 'w') as f:
         f.write(SECRET_KEY)
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/var/db/oi-bus/db.sqlite3',
+        'NAME': '/var/lib/oi-bus/db.sqlite3',
     },
 }
 TEAPOT = '/usr/share/oi-bus/memes'
-STATIC_ROOT = '/run/oi-bus/static'
-MEDIA_ROOT = '/var/db/oi-bus/files'
+STATIC_ROOT = '/var/lib/oi-bus/static'
+MEDIA_ROOT = '/var/lib/oi-bus/files'
 
 # to enable old-school self-registration, change to True
 EVERYONE_IS_ADMIN = False
