e4430701e5541c5af80c2ce0ee0d6aaffad7b8e5
Wojciech Dubiel <w.dubiel@fri.edu.pl>
2020-01-17 11:30:46 +0100

    fixed templates packaging

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ setuptools.setup(
     name='oi_bus',
     version='0.1.0',
     packages=setuptools.find_packages(),
+    package_data={
+        '': ['templates/*.html'],
+    },
     install_requires=['Django>=1.11,<1.12', 'django-macaddress'],
 )
