eac8e8811ce52e0cab9c73a31838922bd287716a
Michał Sidor (Michcioperz) <m.sidor@fri.edu.pl>
2020-01-17 11:38:58 +0100

    package locales

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version='0.1.0',
     packages=setuptools.find_packages(),
     package_data={
-        '': ['templates/*.html'],
+        '': ['templates/*.html', 'locale/*/LC_MESSAGES/*.po', 'locale/*/LC_MESSAGES/*.mo'],
     },
     install_requires=['Django>=1.11,<1.12', 'django-macaddress'],
 )
