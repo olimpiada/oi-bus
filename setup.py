52c5f853a3cef75729854c9a26bd130207063071
Wojciech Dubiel <w.dubiel@fri.edu.pl>
2020-02-03 19:22:39 +0100

    Chown /var/lib/oi-bus in postinst just to be sure

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='oi_bus',
-    version='0.1.12',
+    version='0.1.13',
     packages=setuptools.find_packages(),
     package_data={
         '': ['templates/*.html', 'locale/*/LC_MESSAGES/*.po', 'locale/*/LC_MESSAGES/*.mo', 'static/*.css'],
